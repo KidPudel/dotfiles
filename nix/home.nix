cf9b903017b7596d4cbcf26c5c227a7ef3880fe6
Iggy Kuperson <i.kupchinenko@gmail.com>
2024-10-09 13:43:35 +0300

    manage zshrc with home manager

--- nix/home.nix
+++ nix/home.nix
@@ -11,12 +11,13 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
+  home.username = "iggysleepy";
+  home.homeDirectory = "/Users/iggysleepy";
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-  ".zshrc".source = "~/dotfiles/zshrc/.zshrc";
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
@@ -38,6 +39,7 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+  ".zshrc".source = ~/dotfiles/zshrc/.zshrc;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
