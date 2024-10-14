3494a055dad050b33640f040df64a8a323f86535
Iggy Kuperson <i.kupchinenko@gmail.com>
2024-10-14 09:20:56 +0300

    added odin support

--- nvim/lua/kidpudel/plugins/lsp.lua
+++ nvim/lua/kidpudel/plugins/lsp.lua
@@ -15,19 +15,20 @@ return {
 			auto_install = true,
 		},
 		-- full control of setup
-		-- config = function()
-		--     require("mason-lspconfig").setup({
-		--         ensure_installed = {
-		--             "lua_ls",
-		--             "gopls",
-		--             "html",
-		--             "jsonls",
-		--             "pyright",
-		--             "dockerls",
-		--             "docker_compose_language_service",
-		--         },
-		--     })
-		-- end,
+		config = function()
+		    require("mason-lspconfig").setup({
+		        ensure_installed = {
+		            "lua_ls",
+		            "gopls",
+                    "ols",
+		            "html",
+		            "jsonls",
+		            "pyright",
+		            "dockerls",
+		            "docker_compose_language_service",
+		        },
+		    })
+		end,
 	},
 
 	-- setup the communication between nvim and lsp
