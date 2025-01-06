80f83b803fec34af38abac0353de5d2ee2ef17b9
Iggy Kuperson <i.kupchinenko@gmail.com>
2025-01-07 00:27:31 +0300

    after mental breakdown, use gruvbox as main, kanagawa-dragon for late sessions, and for detox just use default

--- nvim/.config/nvim/lua/kidpudel/plugins/lualine.lua
+++ nvim/.config/nvim/lua/kidpudel/plugins/lualine.lua
@@ -7,7 +7,7 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				theme = "gruvbox",
+				-- theme = "kanagawa-",
 				section_separators = { left = "", right = "" },
 				component_separators = { left = "|", right = "|" },
 			},
--- nvim/.config/nvim/lua/kidpudel/plugins/themes.lua
+++ nvim/.config/nvim/lua/kidpudel/plugins/themes.lua
@@ -1,4 +1,7 @@
 return {
+	-- if this theme is works for you, no need to second guessing the color scheme and waste time on this bs.
+	-- It’s a good to always be evolving, always willing to let go of the comfortable old way to discover a better new way.
+	-- main
 	{
 		"ellisonleao/gruvbox.nvim",
 		priority = 1000,
@@ -8,5 +11,9 @@ return {
 			})
 			vim.cmd("colorscheme gruvbox")
 		end,
+	},
+	-- for late sessions
+	{
+		"rebelot/kanagawa.nvim",
 	},
 }
